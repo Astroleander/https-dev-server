52229f3ddc5bf9d910553e1cd201d02cc5a79cdc
Astroleander <astroleander@gmail.com>
2021-03-23 12:34:20 +0800

    HTTP now

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let entry = document.getElementsByTagName('body');
 entry &&= entry[0];
 
 const title = document.createElement('h1');
-title.innerHTML = 'Hello HTTP';
+title.innerHTML = 'Hello HTTPS';
 
 entry.appendChild(title);
